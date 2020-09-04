5c2673414a1f1a41665f3c1bfc241eb8490d957a
Felipe Magno <philippos.magnum@gmail.com>
2020-09-04 19:20:42 -0300

    .

---
+++ Assets/Playground/Licoes/ForceMode/CameraLookAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    Transform myCamera;
+    [SerializeField] Transform LookTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (LookTarget && myCamera)
+        {
+            myCamera.LookAt(LookTarget);
+        }
+    }
+}
