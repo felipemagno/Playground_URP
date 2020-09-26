92a024d7ba19ade256c1d93f7cbd8155792d4f98
Felipe Magno <philippos.magnum@gmail.com>
2020-09-25 22:35:30 -0300

    Animações e pulo Coyote

---
+++ Assets/Playground/Tutoriais/Movimento2D/Codigo/AnimacaoPlataforma2D.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
+public class AnimacaoPlataforma2D : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    // Armazena o ID do parâmetro "Andando"
+    int hashAndando = Animator.StringToHash("Andando");
+
+    // Armazenar ID dos estados
+    int hashAndar = Animator.StringToHash("Andar");
+    int hashPular = Animator.StringToHash("Pular");
+    int hashIdle = Animator.StringToHash("Idle");
+
+    bool estaPulando = false;
+    bool estaAndando = false;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void Flip(bool viradoDireita)
+    {
+        spriteRenderer.flipX = !viradoDireita;
+    }
+
+    public void Andar(bool andando)
+    {
+        estaAndando = andando;
+        AtualizarAnimacao();
+
+        ////animator.SetBool("Andando", andando);
+        //animator.SetBool(hashAndando, andando);
+
+        //if (andando)
+        //{
+        //    animator.Play("Andar");            
+        //}
+        //else
+        //{
+        //    animator.Play("Idle");
+        //}
+    }
+
+    public void Pular(bool pulando)
+    {
+        estaPulando = pulando;
+        AtualizarAnimacao();
+    }
+
+    private void AtualizarAnimacao()
+    {
+        if (estaPulando)
+        {
+            animator.Play(hashPular);
+            //animator.Play("Pular");
+        }
+        else if (estaAndando)
+        {
+            animator.Play(hashAndar);
+            //animator.Play("Andar");
+        }
+        else
+        {
+            animator.Play(hashIdle);
+            //animator.Play("Idle");
+        }
+    }
+}
--- Assets/Playground/Tutoriais/Movimento2D/Codigo/MovimentoPlataforma2D.cs
+++ Assets/Playground/Tutoriais/Movimento2D/Codigo/MovimentoPlataforma2D.cs
@@ -7,6 +7,7 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
     Transform _transform;
     Rigidbody2D _rigidbody2D;
     VerificarChao _verificarChao;
+    AnimacaoPlataforma2D animacaoPlataforma;
 
     private EPular estadoPulo = EPular.podePular;
 
@@ -20,6 +21,7 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
 
     Vector2 direcao = Vector2.zero;
     bool estaMovendo = false;
+    bool viradoDireita = true;
 
     [Header("Pulo")]
     [SerializeField]
@@ -30,6 +32,11 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
     float duracaoPulo = 0.5f;
     [SerializeField, Tooltip("amplifica a gravidade durante a queda")]
     float multiplicadorQueda = 2f;
+
+    // Variáveis para o "Coyote Jump"
+    bool PuloCoyote = false;
+    [SerializeField] float DuracaoCoyote = 0.4f;
+    Coroutine coroutineCoyote;
 
     Coroutine rotinaPulo;
     float gravidadeInicial;
@@ -48,6 +55,8 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
 
         //Procura pelo componente do tipo Script e de tipo "VerificarChao"
         _verificarChao = GetComponentInChildren<VerificarChao>();
+
+        animacaoPlataforma = GetComponent<AnimacaoPlataforma2D>();
     }
 
     private void FixedUpdate()
@@ -66,6 +75,18 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
     {
         this.direcao = direcao;
         estaMovendo = direcao.sqrMagnitude > 0.1f;
+
+        // verificar direção da animação
+        if (estaMovendo)
+        {
+            viradoDireita = (direcao.x >= 0);
+            animacaoPlataforma.Flip(viradoDireita);
+            animacaoPlataforma.Andar(true);
+        }
+        else
+        {
+            animacaoPlataforma.Andar(false);
+        }
     }
 
     private void Mover()
@@ -89,10 +110,26 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
     // Tente dar o comando de iniciar o pulo
     public void IniciarPulo()
     {
-        if (_verificarChao.EstaNoChao && estadoPulo == EPular.podePular)
+        if (PossoPular())
         {
             estadoPulo = EPular.pular;
         }
+    }
+
+    private bool PossoPular()
+    {
+        bool resultado = _verificarChao.EstaNoChao && estadoPulo == EPular.podePular;
+
+        // Lógica Coyote Jump
+        if (!resultado && PuloCoyote)
+        {
+            if (coroutineCoyote != null) StopCoroutine(coroutineCoyote);
+            PuloCoyote = false;
+            print("COYOTE!");
+            return true;
+        }
+
+        return resultado;
     }
 
     // Avise para parar o pulo
@@ -109,6 +146,13 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
                 break;
         }
         StopCoroutine(rotinaPulo);
+    }
+
+    private IEnumerator InicioCoyote()
+    {
+        PuloCoyote = true;
+        yield return new WaitForSeconds(DuracaoCoyote);
+        PuloCoyote = false;
     }
 
     // Temporizador para avisar a parar de pular mesmo segurando botão de pulo
@@ -128,6 +172,14 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
                 if (!_verificarChao.EstaNoChao)
                 {
                     estadoPulo = EPular.caindo;
+
+                    // Temporario, precisaria de uma animação dele caindo
+                    animacaoPlataforma.Pular(true);
+
+                    // Lógica Coyote Jump
+                    if (coroutineCoyote != null) StopCoroutine(coroutineCoyote);
+                    coroutineCoyote = StartCoroutine(InicioCoyote());
+
                     _rigidbody2D.gravityScale = gravidadeInicial * multiplicadorQueda;
                 }
                 break;
@@ -136,15 +188,17 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
                 _rigidbody2D.AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
                 rotinaPulo = StartCoroutine(TemporizadorPulo());
                 estadoPulo = EPular.pulando;
+                animacaoPlataforma.Pular(true);
                 break;
             // Enquanto segurar botão de pulo, continue pulando
             case EPular.pulando:
-                _rigidbody2D.AddForce(Vector2.up * forcaPulo, ForceMode2D.Force);                
+                _rigidbody2D.AddForce(Vector2.up * forcaPulo, ForceMode2D.Force);
                 break;
             // Quando estiver caindo, verifique se encontrou o chão
             case EPular.caindo:
                 if (_verificarChao.EstaNoChao)
                 {
+                    animacaoPlataforma.Pular(false);
                     estadoPulo = EPular.podePular;
                     _rigidbody2D.gravityScale = gravidadeInicial;
                 }
@@ -163,7 +217,7 @@ public class MovimentoPlataforma2D : MonoBehaviour, IMovimento
         {
             velocidadeCorrigida.x = Mathf.Clamp(velocidadeCorrigida.x, -velocidade, velocidade);
         }
-        
+
         // verificar velocidade maxima de subida no pulo
         if (velocidadeCorrigida.y > velocidadePulo)
         {
